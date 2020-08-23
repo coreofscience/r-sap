061dceb1ca02ec8b3829d61a495ce2b927d9b1c9
srobledog <sebastian.robledo@gmail.com>
2020-08-23 14:02:10 +0000

    Some descriptions details added

--- R/load.R
+++ R/load.R
@@ -1,10 +1,20 @@
-#' @title read isi files
-#' @description this function uses convert2df from bibliometrics package to 
-#' convert the isi file (txt) in a data frame
-#' @param file vector de datos cuyo histograma se va a calcular
-#' @return a isi file in a dataframe format
-#' @export isi_df
+#' @title Loading isi files
+#' 
+#' @description Load isi file (txt) and convert it to a data frame with a bibliometrix
+#' function called convert2df().
+#' 
+#' @param file the isi file (txt) downloaded from Web of Science
+#' 
+#' @import bibliometrix
+#' 
+#' @importFrom bibliometrix convert2df
+#' 
+#' @return a dataframe with all variables in columns
+#' 
 #' @examples
+#' 
 isi_df <- function(file) {
-  isi_file = bibliometrix::convert2df(file = file, dbsource = "wos", format = "plaintext")
+  isi_file = bibliometrix::convert2df(file = file, 
+                                      dbsource = "wos", 
+                                      format = "plaintext")
 } 
