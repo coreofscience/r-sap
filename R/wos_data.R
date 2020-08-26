e979b5f5a24392d2bc8c4ba7b2b438d65fa3ddaf
srobledog <sebastian.robledo@gmail.com>
2020-08-26 14:01:57 +0000

    name was changed

---
+++ R/wos_data.R
@@ -0,0 +1,10 @@
+#' Web of Science Data
+#' 
+#' A dataset containing a search of Web of Science
+#' 
+#' @format a txt file 
+#' \describe{
+#'  \item{TI}{Title of the papers}
+#'  }
+#'  @source Web of Science
+"wos"
