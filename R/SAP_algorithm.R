b430b6e9b6fc49784a1efa7f5ba359c4dd9b8430
Luis Alexander Valencia <61251207+alexander-valencia-control@users.noreply.github.com>
2020-09-09 21:28:38 -0500

    add Ejemplo de algoritmo SAP en R

--- R/SAP_algorithm.R
+++ R/SAP_algorithm.R
@@ -1,13 +1,13 @@
-# Este programa es para la implementacion del algoritmo SAP para analisis de 
-# citaciones. 
+# Este programa es para la implementacion del algoritmo SAP para analisis de
+# citaciones.
 # ------------------------------------------------------------------------------
-# Referencias: 
-# 1) SAP algorithm for citation analisis: an improvement to tree of science. 
-# Daniel S. Valencia, Sebastian Robledo, Ricardo Pinilla, Nestor D. Duque y 
-# Gerard Olivar Tost. Revista de Ingenieria e Investigacion. 2020. 
+# Referencias:
+# 1) SAP algorithm for citation analisis: an improvement to tree of science.
+# Daniel S. Valencia, Sebastian Robledo, Ricardo Pinilla, Nestor D. Duque y
+# Gerard Olivar Tost. Revista de Ingenieria e Investigacion. 2020.
 # ------------------------------------------------------------------------------
-# Elaboro: Luis Alexander Valencia - 08 septiembre 2020 
-# correo: lavalenciah@unal.edu.co - lavalenciah12@gmail.com 
+# Elaboro: Luis Alexander Valencia - 08 septiembre 2020
+# correo: lavalenciah@unal.edu.co - lavalenciah12@gmail.com
 
 # Se cargan las librerias necesarias
 
@@ -18,11 +18,11 @@ library(bibliometrix)
 library(ggplot2)
 library(CINNA)
 
-# Se cargan los datos 
+# Se cargan los datos
 
 nombre.archivo  <- "savedrecs - 2020-08-29T093831.545.txt"
-data_wos        <- convert2df(file     = nombre.archivo, 
-                              dbsource = "wos", 
+data_wos        <- convert2df(file     = nombre.archivo,
+                              dbsource = "wos",
                               format   = "plaintext")
 
 # Se Crea el ID_WOS para compararlo con las referencias
@@ -50,32 +50,32 @@ data_wos$ID_WOS <- ifelse(!is.na(data_wos$DI),
 
 # Creando la lista de enlases
 
-edgelist <- 
-  as_tibble(data_wos) %>% 
-  mutate(cited_references = CR) %>% 
+edgelist <-
+  as_tibble(data_wos) %>%
+  mutate(cited_references = CR) %>%
   separate_rows(CR, sep = ";") %>%
   filter(!grepl(pattern = "^[0-9].*",
-                CR)) %>% 
-  select(ID_WOS, CR) %>% 
-  filter(CR != "" & is.na(CR) == FALSE) %>% 
+                CR)) %>%
+  select(ID_WOS, CR) %>%
+  filter(CR != "" & is.na(CR) == FALSE) %>%
   mutate(ID_WOS = str_to_upper(ID_WOS),
-         CR = str_to_upper(CR)) %>% 
+         CR = str_to_upper(CR)) %>%
   unique()
 
 # Se crea el grafo y se eliminan los bucles o ciclos y las aristas repetidas
-graph <- graph.data.frame(edgelist) %>% 
-         simplify()
+graph <- graph.data.frame(edgelist) %>%
+  simplify()
 
 # Se eliminan los vertices con indegree = 1 y con outdegree = 0
-graph_1 <- delete.vertices(graph, 
-                           which(degree(graph, mode = "in") == 1 & 
-                           degree(graph, mode = "out") == 0))
+graph_1 <- delete.vertices(graph,
+                           which(degree(graph, mode = "in") == 1 &
+                                   degree(graph, mode = "out") == 0))
 
-# Se escoge el componente mas grande conectado 
+# Se escoge el componente mas grande conectado
 graph_2 <- giant_component_extract(graph_1, directed = TRUE)
 graph_2 <- graph_2[[1]]
 
-# Se determinan las metricas de la red 
+# Se determinan las metricas de la red
 
 metricas.red <- tibble(
   id        = V(graph_2)$name,
@@ -83,29 +83,61 @@ metricas.red <- tibble(
   outdegree = degree(graph_2, mode = "out"),
   bet       = betweenness(graph_2))
 
+metricas.red <- metricas.red %>%
+  mutate(year = as.numeric(str_extract(id, "[0-9]{4}")))
+
 # Clasificacion de las raices
 
 Raices <- metricas.red[metricas.red$outdegree == 0, c("id","indegree")] %>%
-          arrange(desc(indegree))
+  arrange(desc(indegree))
 Raices <- Raices[1:10,]
 
 
-# Clasificacion de las hojas 
-Nodos.grafo <- as.list(V(graph_2))
-Hojas.ext <- metricas.red[metricas.red$indegree == 0, c("id","outdegree")]
-HOjas.new <- filter()
+# Clasificacion de las hojas
+Hojas.ext <- metricas.red[metricas.red$indegree == 0, c("id","outdegree","year")]
+act.year  <- as.numeric(format(Sys.Date(),'%Y'))
+Hojas.ext <- Hojas.ext %>%
+  mutate(antiguedad = act.year - year) %>%
+  arrange(antiguedad)
+Hojas     <- filter(Hojas.ext, antiguedad <= 5)
 
-h <- get.all.shortest.paths(graph_2, from = 50, to = 6:914, mode = c("out"))
+# Se determina el numero del vertice de las Hojas
+num.vertices.hojas <- c()
+for (vertice in Hojas$id){
+  num.vertices.hojas <- c(num.vertices.hojas,which(metricas.red$id == vertice))
+}
+
+# Se determina el numero del vertice de las raices
+num.vertices.raices <- c()
+for (vertice in Raices$id){
+  num.vertices.raices <- c(num.vertices.raices,which(metricas.red$id == vertice))
+}
+
+# Calculo del SAP de las Hojas
+SAP_hojas <- c()
+Caminos   <- c()
+for (vert in Hojas$id){
+  h <- get.all.shortest.paths(graph_2,
+                              from = vert,
+                              to   = Raices$id,
+                              mode = "out")
+
+  SAP_hojas   <- c(SAP_hojas, length(h[[1]]))
+  lista.nodos <- unique(unlist(h[1]))
+  lista.nodos <- lista.nodos[!(lista.nodos %in% num.vertices.raices)]
+  lista.nodos <- lista.nodos[!(lista.nodos %in% num.vertices.hojas)]
+  Caminos     <- c(Caminos,lista.nodos)
+}
+Hojas <- Hojas %>%
+  mutate(SAP = SAP_hojas) %>%
+  arrange(desc(SAP))
+
+Hojas <- Hojas[1:60,]
 
 
-names <- data_wos$ID_WOS
+# Seleccion del tronco
 
-tos <- tos %>% mutate(year = str_extract(id, "[0-9]{4}"))
-
-
-
-
-
-
-
-
+Tronco     <- metricas.red[Caminos, c("id","indegree","year")]
+mas.nuevo  <- max(Tronco$year, na.rm = TRUE)
+Tronco     <- Tronco %>%
+  mutate(antiguedad = mas.nuevo - year)
