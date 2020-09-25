f12c01529a827945bf5be04027ac762e023132e4
Luis Alexander Valencia <61251207+alexander-valencia-control@users.noreply.github.com>
2020-09-25 16:04:27 -0500

    add sap_tos

--- R/sap_process.R
+++ R/sap_process.R
@@ -17,38 +17,89 @@
 #' @importFrom utils head
 
 sap_process <- function(graph_file) {
-  
-  network.metrics <-
-    dplyr::tibble(
-      id = igraph::V(graph_file)$name,
-      indegree = igraph::degree(graph_file, mode = "in"),
-      outdegree = igraph::degree(graph_file, mode = "out"),
-      bet = igraph::betweenness(graph_file)
-    )
-  
-  seminals <- network.metrics[network.metrics$outdegree == 0,
-                              c("id","indegree")]
-  seminals <- utils::head(seminals[with(seminals, order(-indegree)),],10)
-  
-  structurals <- network.metrics[network.metrics$bet > 0,
-                                 c("id", "bet")]
-  structurals <- utils::head(structurals[with(structurals, order(-bet)),],10)
-  
-  current <- network.metrics[network.metrics$indegree == 0,
-                             c("id","outdegree")]
-  current <- utils::head(current[with(current, order(-outdegree)),], 60)
-  
-  if(sum(network.metrics$bet) == 0 ) {
-    stop("Your ToS does not have trunk, check the search out")} else {
-      
-      
-      seminals$ToS <- "Raiz"
-      seminals$order <- 1:length(seminals$id)
-      structurals$ToS <- "Tronco"
-      structurals$order <- 1:length(structurals$id)
-      current$ToS <- "Hojas"
-      current$order <- 1:length(current$id)
-      
-      tos <- rbind(seminals[,c(1,3,4)], structurals[,c(1,3,4)], current[,c(1,3,4)])
-    }
+  # Se crean la metricas de la red
+
+  metricas.red <- tibble(
+    id        = V(graph_2)$name,
+    indegree  = degree(graph_2, mode = "in"),
+    outdegree = degree(graph_2, mode = "out"),
+    bet       = betweenness(graph_2))
+
+  metricas.red <- metricas.red %>%
+    mutate(year = as.numeric(str_extract(id, "[0-9]{4}")))
+
+  # Clasificacion de las raices
+
+  Raices <- metricas.red[metricas.red$outdegree == 0, c("id","indegree")] %>%
+    arrange(desc(indegree))
+  Raices <- Raices[1:10,]
+
+
+  # Clasificacion de las hojas
+  Hojas.ext <- metricas.red[metricas.red$indegree == 0, c("id","outdegree","year")]
+  act.year  <- as.numeric(format(Sys.Date(),'%Y'))
+  Hojas.ext <- Hojas.ext %>%
+    mutate(antiguedad = act.year - year) %>%
+    arrange(antiguedad)
+  Hojas     <- filter(Hojas.ext, antiguedad <= 5)
+
+  # Se determina el numero del vertice de las Hojas
+  num.vertices.hojas <- c()
+  for (vertice in Hojas$id){
+    num.vertices.hojas <- c(num.vertices.hojas,which(metricas.red$id == vertice))
+  }
+
+  # Se determina el numero del vertice de las raices
+  num.vertices.raices <- c()
+  for (vertice in Raices$id){
+    num.vertices.raices <- c(num.vertices.raices,which(metricas.red$id == vertice))
+  }
+
+  # Calculo del SAP de las Hojas
+  SAP_hojas <- c()
+  for (vert in Hojas$id){
+    h <- get.all.shortest.paths(graph_2,
+                                from = vert,
+                                to   = Raices$id,
+                                mode = "out")
+
+    SAP_hojas   <- c(SAP_hojas, length(h[[1]]))
+  }
+  Hojas <- Hojas %>%
+    mutate(SAP = SAP_hojas) %>%
+    arrange(desc(SAP))
+
+  Hojas <- Hojas[1:60,] %>%
+    filter(SAP > 0)
+
+  Caminos   <- c()
+  for (vert in Hojas$id){
+    h <- get.all.shortest.paths(graph_2,
+                                from = vert,
+                                to   = Raices$id,
+                                mode = "out")
+    lista.nodos <- unique(unlist(h[1]))
+    lista.nodos <- lista.nodos[!(lista.nodos %in% num.vertices.raices)]
+    lista.nodos <- lista.nodos[!(lista.nodos %in% num.vertices.hojas)]
+    Caminos     <- c(Caminos,lista.nodos)
+  }
+
+
+
+  # Seleccion del tronco
+
+  Tronco     <- metricas.red[unique(Caminos), c("id","indegree","year")]
+  mas.nuevo  <- max(Tronco$year, na.rm = TRUE)
+  Tronco     <- Tronco %>%
+    mutate(antiguedad = mas.nuevo - year)
+
+
+
+  # Tree of science
+  Raices$TOS <- "Raiz"
+  Hojas$TOS  <- "Hojas"
+  Tronco$TOS <- "Tronco"
+
+  tos       <- rbind(Raices[,c(1,3)], Tronco[,c(1,5)], Hojas[,c(1,6)])
+  return(tos)
 }
