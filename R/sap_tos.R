a89b59ad3417912ee09919e6025fed1171d20b9e
Lizet Gomez <algomezb@unal.edu.co>
2020-10-25 23:53:54 -0500

    new changes

--- R/sap_tos.R
+++ R/sap_tos.R
@@ -5,19 +5,17 @@
 #' sap algorithm represents the metaphor of the
 #' tree sap
 #'
-#' @param file the isi file (txt) downloaded from web of science
-#'
 #' @author Sebastian Robledo
-#'
+#' @param file the isi file (txt) downloaded from web of science
 #' @return a dataframe with roots, trunk, and leaves
 #'
 #' @export
 
 sap_tos <- function(file) {
-  
+
   isi_file <- sap_load(file)
-  
+
   graph_file <- sap_graph(isi_file)
-  
+
   sap_file <- sap_process(graph_file)
 }
