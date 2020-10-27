9252a2d07890b3cf9c3d9af6483e37727b4414ac
Lizet Gomez <algomezb@unal.edu.co>
2020-10-26 23:26:00 -0500

    warning resulted

--- R/sap_process.R
+++ R/sap_process.R
@@ -9,46 +9,48 @@
 #'
 #' @author Sebastian Robledo
 #'
-#' @import dplyr
-#' @import igraph
-#' @import utils
+#' @importFrom dplyr tibble
+#' @importFrom igraph V
+#' @importFrom igraph degree
+#' @importFrom igraph betweenness
+#' @importFrom utils head
 #'
 #' @return a graph object
-#' @importFrom utils head
+
 
 sap_process <- function(graph_file) {
-  
+
   network.metrics <-
-    dplyr::tibble(
-      id = igraph::V(graph_file)$name,
-      indegree = igraph::degree(graph_file, mode = "in"),
-      outdegree = igraph::degree(graph_file, mode = "out"),
-      bet = igraph::betweenness(graph_file)
+    tibble(
+      id = V(graph_file)$name,
+      indegree = degree(graph_file, mode = "in"),
+      outdegree = degree(graph_file, mode = "out"),
+      bet = betweenness(graph_file)
     )
-  
+
   seminals <- network.metrics[network.metrics$outdegree == 0,
                               c("id","indegree")]
-  seminals <- utils::head(seminals[with(seminals, order(-indegree)),],10)
-  
+  seminals <- head(seminals[with(seminals, order(-indegree)),],10)
+
   structurals <- network.metrics[network.metrics$bet > 0,
                                  c("id", "bet")]
-  structurals <- utils::head(structurals[with(structurals, order(-bet)),],10)
-  
+  structurals <- head(structurals[with(structurals, order(-bet)),],10)
+
   current <- network.metrics[network.metrics$indegree == 0,
                              c("id","outdegree")]
-  current <- utils::head(current[with(current, order(-outdegree)),], 60)
-  
+  current <- head(current[with(current, order(-outdegree)),], 60)
+
   if(sum(network.metrics$bet) == 0 ) {
     stop("Your ToS does not have trunk, check the search out")} else {
-      
-      
+
+
       seminals$ToS <- "Raiz"
       seminals$order <- 1:length(seminals$id)
       structurals$ToS <- "Tronco"
       structurals$order <- 1:length(structurals$id)
       current$ToS <- "Hojas"
       current$order <- 1:length(current$id)
-      
+
       tos <- rbind(seminals[,c(1,3,4)], structurals[,c(1,3,4)], current[,c(1,3,4)])
     }
 }
